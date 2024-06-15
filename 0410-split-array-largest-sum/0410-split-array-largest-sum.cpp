628d2bc1016521cd725dd0bb6619a0f2e78baa68
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-15 18:54:54 -0500

    Time: 4 ms (42.22%), Space: 9.2 MB (34.15%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int numsOfSubArray(vector<int>& nums, int sum) {
+
+        // Binary Search
         int numsOfSubArray = 1;
         int sumOfSubArr = 0;
         for (int i = 0; i < nums.size(); i++) {
@@ -24,13 +26,42 @@ public:
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (numsOfSubArray(nums, mid) <= k) {
-                high = mid - 1;
+            if (numsOfSubArray(nums, mid) <= k) { // check if numsOfSubArray is less the. the given array then
+                high = mid - 1; // remove right portion of array
             } else {
-                low = mid + 1;
+                low = mid + 1; // remove left portion of array
             }
         }
 
         return low;
     }
+// +++++++++++++++ Brute Force Approch +++++++++++
+//     int countPartitions(vector<int> &a, int maxSum) {
+//     int n = a.size(); //size of array.
+//     int partitions = 1;
+//     long long subarraySum = 0;
+//     for (int i = 0; i < n; i++) {
+//         if (subarraySum + a[i] <= maxSum) {
+//             //insert element to current subarray
+//             subarraySum += a[i];
+//         }
+//         else {
+//             //insert element to next subarray
+//             partitions++;
+//             subarraySum = a[i];
+//         }
+//     }
+//     return partitions;
+// }
+
+// int largestSubarraySumMinimized(vector<int> &a, int k) {
+//     int low = *max_element(a.begin(), a.end());
+//     int high = accumulate(a.begin(), a.end(), 0);
+
+//     for (int maxSum = low; maxSum <= high; maxSum++) {
+//         if (countPartitions(a, maxSum) == k)
+//             return maxSum;
+//     }
+//     return low;
+// }
 };
