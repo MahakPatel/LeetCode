1568e8aa0328f672a201c8bdf8fc118772251f36
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-24 16:51:48 -0500

    Time: 5 ms (30.91%), Space: 11.9 MB (6.83%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.cpp
+++ 0206-reverse-linked-list/0206-reverse-linked-list.cpp
@@ -11,16 +11,77 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        ListNode* temp = head; // current node
-        ListNode* back = NULL; // previous node of current node
 
-        while(temp != NULL){
-           ListNode* front = temp->next; // next node of current node
-            temp -> next = back;
-            back = temp;
-            temp = front;
-        }
+        //+++++++++++++++++ Brute Force Approch +++++++++++++++
 
-        return back;
+        //     // Create a temporary pointer
+        // // to traverse the linked list
+        // ListNode* temp = head;
+
+        // // Create a stack to temporarily
+        // // store the data values
+        // stack<int> st;
+
+        // // Step 1: Push the values of the
+        // // linked list onto the stack
+        // while (temp != nullptr) {
+        //     // Push the current node's
+        //     // data onto the stack
+        //     st.push(temp->val);
+
+        //     // Move to the next node
+        //     // in the linked list
+        //     temp = temp->next;
+        // }
+
+        // // Reset the temporary pointer to
+        // // the head of the linked list
+        // temp = head;
+
+        // // Step 2: Pop values from the stack
+        // // and update the linked list
+        // while (temp != nullptr) {
+        //     // Set the current node's data to
+        //     // the value at the top of the stack
+        //     temp->val = st.top();
+
+        //     // Pop the top element from the stack
+        //     st.pop();
+
+        //     // Move to the next node
+        //     // in the linked list
+        //     temp = temp->next;
+        // }
+
+        // // Return the new head of
+        // // the reversed linked list
+        // return head;
+
+        //
+        // ++++++++ Optimal solution +++++++++++++
+        // ListNode* temp = head; // current node
+        // ListNode* back = NULL; // previous node of current node
+
+        // while (temp != NULL) {
+        //     ListNode* front = temp->next; // next node of current node
+        //     temp->next = back;
+        //     back = temp;
+        //     temp = front;
+        // }
+
+        // return back;
+
+           // +++++++++++++ Recursive Approch ++++++++++++
+
+    if (head == NULL || head -> next == NULL) return head;
+
+    ListNode* newHead = reverseList(head -> next);
+    ListNode* front = head -> next;
+    front -> next = head;
+    head -> next = NULL;
+
+    return newHead;
+
+
     }
 };
