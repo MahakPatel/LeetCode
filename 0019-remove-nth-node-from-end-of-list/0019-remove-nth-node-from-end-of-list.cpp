c3b518351b9a8272f6992685063d555b498529cc
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-26 18:44:49 -0500

    Time: 0 ms (100%), Space: 13.3 MB (20.24%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -12,39 +12,64 @@ class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
 
-        // +++++++++++++++ Brute Force ++++++++++++ TC O(2N). SC O(1)
-        int count =0;
-        ListNode* temp = head;
+        ListNode* fast = head;
+        ListNode* slow = head;
 
-        while(temp != NULL){ // find the length of LL
-            count++;
-            temp = temp -> next;
+        for(int i=0;i<n;i++){ // move fast till given n
+            fast = fast->next; 
         }
 
-        if(count == n ){ // if n == length of LL means the head of the LL then make the head->next is a new Head
+        if(fast == NULL){ // if n= length of LL then return second node as newHead
             ListNode* newHead = head -> next;
             delete(head);
-
             return newHead;
         }
 
-        int resultNode = count - n; // total minus given number
-
-        temp = head;
-
-        while(temp!=NULL){
-            resultNode--;
-
-            if(resultNode == 0) break;
-
-            temp = temp->next;
+        while(fast->next != NULL){ // now move fast and slow by one node
+            slow = slow->next;
+            fast = fast->next;
         }
 
-        ListNode* deletedNode = temp -> next; // for delete 
+        ListNode* deletedNode = slow-> next; // save the delete node
+        slow->next = slow->next->next; // connnect previous node of delete node with next node of dleted node
 
-        temp->next = temp->next->next; // previous node of deleted nod connect to next Node of deleted node
         delete(deletedNode);
-
+        
         return head;
+
+        // +++++++++++++++ Brute Force ++++++++++++ TC O(2N). SC O(1)
+        // int count =0;
+        // ListNode* temp = head;
+
+        // while(temp != NULL){ // find the length of LL
+        //     count++;
+        //     temp = temp -> next;
+        // }
+
+        // if(count == n ){ // if n == length of LL means the head of the LL then make the head->next is a new Head
+        //     ListNode* newHead = head -> next;
+        //     delete(head);
+
+        //     return newHead;
+        // }
+
+        // int resultNode = count - n; // total minus given number
+
+        // temp = head;
+
+        // while(temp!=NULL){
+        //     resultNode--;
+
+        //     if(resultNode == 0) break;
+
+        //     temp = temp->next;
+        // }
+
+        // ListNode* deletedNode = temp -> next; // for delete 
+
+        // temp->next = temp->next->next; // previous node of deleted nod connect to next Node of deleted node
+        // delete(deletedNode);
+
+        // return head;
     }
 };
