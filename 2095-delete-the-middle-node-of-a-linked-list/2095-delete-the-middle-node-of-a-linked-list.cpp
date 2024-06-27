5e21dcc4ff336c8971fa31aa8eac2c2b79c99ec1
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-27 01:53:06 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -0,0 +1,62 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteMiddle(ListNode* head) {
+
+        if(head == NULL and head->next == NULL){
+            return NULL;
+        }
+
+        ListNode* slow = head; // we need to move slow previous of middle mode so we will skip one interation of slow
+        ListNode* fast = head; // move 2 node at time
+
+        fast = fast->next->next; // just move fast and skip 1 interation of slow
+
+        while (fast != NULL && fast->next != NULL) {
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+
+        ListNode* deletedNode = slow->next; // save deleted node
+        slow->next = slow->next->next; // connect previous node with next node of delete
+        delete (deletedNode);
+
+        return head;
+
+        // +++++++++++++++ Brute Force +++++++ TC O(N + N/2) SC O(1)
+
+        // int n = 0;
+        // ListNode* temp = head;
+
+        // while (temp != NULL) { // fing the length of LL
+        //     n++;
+        //     temp = temp->next;
+        // }
+
+        // temp = head;
+
+        // int mid = n / 2; // mid = length of LL /2
+        // while (temp != NULL) { // move temp till second last node of middle
+        // one
+        //     mid--;
+        //     if (mid == 0) {
+        //         ListNode* deletedNode = temp->next; // save the deleted node
+        //         temp->next = temp->next->next; // connect previous node to
+        //         next node of deleted node delete (deletedNode); break;
+        //     }
+
+        //     temp = temp->next;
+        // }
+
+        // return head;
+    }
+};
