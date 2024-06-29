4af19b990dcb82d62585ddc5f65629824928279b
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-06-29 15:30:57 -0500

    Time: 7 ms (19.18%), Space: 13.4 MB (20.23%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -15,26 +15,28 @@ public:
         ListNode* fast = head;
         ListNode* slow = head;
 
-        for(int i=0;i<n;i++){ // move fast till given n
-            fast = fast->next; 
+        for (int i = 0; i < n; i++) { // move fast till given n
+            fast = fast->next;
         }
 
-        if(fast == NULL){ // if n= length of LL then return second node as newHead
-            ListNode* newHead = head -> next;
-            delete(head);
+        if (fast ==
+            NULL) { // if n= length of LL then return second node as newHead
+            ListNode* newHead = head->next;
+            delete (head);
             return newHead;
         }
 
-        while(fast->next != NULL){ // now move fast and slow by one node
+        while (fast->next != NULL) { // now move fast and slow by one node
             slow = slow->next;
             fast = fast->next;
         }
 
-        ListNode* deletedNode = slow-> next; // save the delete node
-        slow->next = slow->next->next; // connnect previous node of delete node with next node of dleted node
+        ListNode* deletedNode = slow->next; // save the delete node
+        slow->next = slow->next->next; // connnect previous node of delete node
+                                       // with next node of dleted node
 
-        delete(deletedNode);
-        
+        delete (deletedNode);
+
         return head;
 
         // +++++++++++++++ Brute Force ++++++++++++ TC O(2N). SC O(1)
@@ -46,7 +48,8 @@ public:
         //     temp = temp -> next;
         // }
 
-        // if(count == n ){ // if n == length of LL means the head of the LL then make the head->next is a new Head
+        // if(count == n ){ // if n == length of LL means the head of the LL
+        // then make the head->next is a new Head
         //     ListNode* newHead = head -> next;
         //     delete(head);
 
@@ -65,10 +68,10 @@ public:
         //     temp = temp->next;
         // }
 
-        // ListNode* deletedNode = temp -> next; // for delete 
+        // ListNode* deletedNode = temp -> next; // for delete
 
-        // temp->next = temp->next->next; // previous node of deleted nod connect to next Node of deleted node
-        // delete(deletedNode);
+        // temp->next = temp->next->next; // previous node of deleted nod
+        // connect to next Node of deleted node delete(deletedNode);
 
         // return head;
     }
