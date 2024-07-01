989b501e9bb19f68608c456dd370c5caec50063e
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-07-01 14:06:38 -0500

    Time: 0 ms (100%), Space: 8.1 MB (21.76%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+
+        // ++++++++++++ Brute force +++++++++++
+
+        // int nn = n;
+
+        // if(nn<0) nn = -1 * nn;
+
+        // double ans = 1.0;
+        // for (int i = 0; i < nn; i++) {
+        //     ans = ans * x;
+        // }
+
+        // if (n < 0) {
+        //     ans = (double)(1.0) / (double)(ans);
+        //     cout<<ans<<"+++++++++";
+        // }
+
+        // return ans;
+
+
+        // ++++++++++++++++ Optimal Solution +++++++++++
+            double ans = 1.0;
+            long long nn = n;
+            if (nn < 0) // is n value is negative then convert into positive
+                nn = -1 * nn;
+            while (nn) { // run till nn value become invalid(0)
+                if (nn % 2) { // if (modulo 2 is 0)
+                    ans = ans * x; // mul ans with value
+                    nn = nn - 1; // ans reduce power by 1
+                } else { // otherwise
+                    x = x * x; // mul value with itself
+                    nn = nn / 2; // and divide power by 2
+                }
+            }
+            if (n < 0) // if n value is negative then ans should be 1/ans;
+                ans = (double)(1.0) / (double)(ans);
+            return ans;
+    }
+};
