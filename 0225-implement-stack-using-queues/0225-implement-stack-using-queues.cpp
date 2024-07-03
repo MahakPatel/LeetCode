d054b0e22874237a9b2d2a39e2f09e54e9df4e57
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-07-03 15:41:41 -0500

    Time: 0 ms (100%), Space: 8.3 MB (12.11%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,33 +1,68 @@
 class MyStack {
 public:
-    MyStack() { }
-    queue<int> q;
 
+// +++++++++++ Using Two Queues  +++++++++++++++
+    queue<int> que1;
+    queue<int> que2;
+
+    MyStack() {}
+    
     void push(int x) {
+        que2.push(x);
 
-        int s = q.size();
-        q.push(x);
-
-        for (int i = 0; i < s; i++) {
-            q.push(q.front());
-            q.pop();
+        while(!que1.empty()){
+            que2.push(que1.front());
+            que1.pop();
         }
-    }
 
+        swap(que1, que2);
+    }
+    
     int pop() {
-        int n = q.front();
-        q.pop();
+        int result = que1.front();
+        que1.pop();
 
-        return n;
+        return result;
     }
-
-    int top() { 
-       return q.front(); 
+    
+    int top() {
+        return que1.front();
     }
-
+    
     bool empty() {
-        return q.empty();
+        return que1.empty();
     }
+
+
+// ++++++++++++ Using One Queues +++++++++++++++++
+    // MyStack() { }
+    // queue<int> q;
+
+    // void push(int x) {
+
+    //     int s = q.size();
+    //     q.push(x);
+
+    //     for (int i = 0; i < s; i++) {
+    //         q.push(q.front());
+    //         q.pop();
+    //     }
+    // }
+
+    // int pop() {
+    //     int n = q.front();
+    //     q.pop();
+
+    //     return n;
+    // }
+
+    // int top() { 
+    //    return q.front(); 
+    // }
+
+    // bool empty() {
+    //     return q.empty();
+    // }
 };
 
 /**
