3609db615ebf06bedb4cda897dfff264105024b6
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-07-10 22:06:47 -0500

    Time: 20 ms (35.19%), Space: 21.1 MB (34.12%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -1,56 +1,85 @@
+#include <iostream>
 class MinStack {
-    stack<int> st;
-    int mini;
-public:
-    MinStack() {
-        while(st.empty() == false){
-            st.pop();
-        }
 
-        mini = INT_MAX; // assign mini the highest int value
+    stack < pair < int, int >> st;
+
+  public:
+    void push(int x) {
+      int min;
+      if (st.empty()) {
+        min = x;
+      } else {
+        min = std::min(st.top().second, x);
+      }
+      st.push({x,min});
     }
+
+  void pop() {
+    st.pop();
+  }
+
+  int top() {
+    return st.top().first;
+  }
+
+  int getMin() {
+    return st.top().second;
+  }
+
+  
+//     stack<long long> st;
+//     long mini;
+// public:
+//     MinStack() {
+//         while(st.empty() == false){
+//             st.pop();
+//         }
+
+//         mini = INT_MAX; // assign mini the highest int value
+//     }
     
-    void push(int val) {
-        if(st.empty()){ // if stack is empty push the element and also assign that value to mini
-            mini = val;
-            st.push(val);
-        }else{
-            if(val < mini){ // if val is minimum of previous val then modify tthe value and push in to stack
-                st.push(2 * val * 1LL - mini);
-                mini = val; // but assign mini a original value only
-            }else{
-                st.push(val);
-            }
-        }
-    }
+//     void push(int value) {
+//       long long val =  Long.valueOf(value);
+//         if(st.empty()){ // if stack is empty push the element and also assign that value to mini
+//             mini = val;
+//             st.push(val);
+//         }else{
+//             if(val < mini){ // if val is minimum of previous val then modify tthe value and push in to stack
+//                 st.push(2 * val * 1LL - mini);
+//                 mini = val; // but assign mini a original value only
+//             }else{
+//                 st.push(val);
+//             }
+//         }
+//     }
     
-    void pop() {
-        if(st.empty()) return;
+//     void pop() {
+//         if(st.empty()) return;
 
-        long long element = st.top(); // top element of the stack
-        st.pop();
+//         long long element = st.top(); // top element of the stack
+//         st.pop();
 
-        if(element < mini){ // if element is smaller then mini then again convert that midify value into original value
-            mini = 2 * mini - element;
-        }
-    }
+//         if(element < mini){ // if element is smaller then mini then again convert that midify value into original value
+//             mini = 2 * mini - element;
+//         }
+//     }
     
-    int top() {
-        if(st.empty()) return -1;
+//     int top() {
+//         if(st.empty()) return -1;
 
-        long long element = st.top();
+//         long long element = st.top();
 
-        if(element < mini){ // if element is smaller then mini return mini
-            return mini;
-        }
+//         if(element < mini){ // if element is smaller then mini return mini
+//             return mini;
+//         }
 
-        return element; // otherwise retuen element
+//         return element; // otherwise retuen element
 
-    }
+//     }
     
-    int getMin() {
-        return mini;
-    }
+//     int getMin() {
+//         return mini;
+//     }
 };
 
 /**
