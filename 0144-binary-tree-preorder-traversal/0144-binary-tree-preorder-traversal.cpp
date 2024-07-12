3bf765a16e99481e2ee2747e4f2b8429b5141428
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-07-12 15:02:42 -0500

    Time: 4 ms (31.65%), Space: 10.1 MB (9.46%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -6,24 +6,51 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
-    void solve(TreeNode *root , vector<int> &ans){
-        if(root == NULL){ // if root equal to zero return 
-            return ;
+
+//++++++++++++++++++++ Recursive +++++++++++++++++
+    void solve(TreeNode* root, vector<int>& ans) {
+        if (root == NULL) { // if root equal to zero return
+            return;
         }
 
-// preorder means (root,left,right)
-        ans.push_back(root -> val); // push the node value into vector
-        solve(root -> left, ans); // call function itself with left node
-        solve(root -> right, ans);// call function itself with right node
+        // preorder means (root,left,right)
+        ans.push_back(root->val); // push the node value into vector
+        solve(root->left, ans);   // call function itself with left node
+        solve(root->right, ans);  // call function itself with right node
     }
     vector<int> preorderTraversal(TreeNode* root) {
-        vector<int> ans ; // create vector to store the answer
-        solve(root , ans);
+        vector<int> ans; // create vector to store the answer
+        solve(root, ans);
         return ans;
     }
+//+++++++++++++++ Itertive +++++++++++++++++
+    // vector<int> preorderTraversal(TreeNode* root) {
+    //     vector<int> preOrder; // for storing ans
+    //     if (root == NULL)
+    //         return preOrder;
+
+    //     stack<TreeNode*> st; // create empty stack
+
+    //     st.push(root); // push root node
+
+    //     while (!st.empty()) {
+    //         root = st.top(); // get top of the stack
+    //         st.pop();
+
+    //         preOrder.push_back(root->val); // push root value into preOrder 
+
+    //         if (root->right != NULL) // if right node is present 
+    //             st.push(root->right); // push that node into stack
+    //         if (root->left != NULL)// if left node is present 
+    //             st.push(root->left);// push that node into stack
+    //     }
+
+    //     return preOrder;
+    // }
 };
