79f23256810af456aaec0442f118cac32e66a1c5
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-07-17 23:01:34 -0500

    Time: 2 ms (44.72%), Space: 12.2 MB (9.45%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.cpp
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSameTree(TreeNode* p, TreeNode* q) {
+        if(p == NULL || q == NULL) return (p==q);
+        // apply any DFS method(here i apply preOrder travesal)
+        return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right); // check each node of p with each node of q at same time
+    }
+};
