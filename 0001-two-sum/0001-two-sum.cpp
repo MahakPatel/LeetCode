869165998a91c62b9fabe52ee20c57db181c923c
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-08-09 21:53:57 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n= nums.size();
+
+        int j=0;
+        for(int i=0;i<n;i++){
+            j++;
+            if(nums[i]+nums[j]==target){
+                return {i,j};
+            }
+        }
+
+        return {};
+        
+    //     for(int i=0;i< n-1;i++){
+    //         for (int j=i+1;j<n;j++){
+    //         if(nums[i]+nums[j]== target){
+    //             return {i,j};
+    //         }
+        
+    //     }
+    // }
+    // return {}; 
+     
+    }
+};
