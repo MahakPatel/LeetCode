ed3003d280764e484a7fdd01a3aa4939a0b6db93
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-08-10 16:03:43 -0500

    Time: 8 ms (77.94%), Space: 14.6 MB (14.13%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // +++++++++++ Optimal Solution ( only if we need to return 'Yes' Or
+        // +++++++++++ Optimal Solution - 2 pointer Approach ( only if we need to return 'Yes' Or
         // 'No') ++++++++
         //     int n = nums.size();;
 
-        //     sort(nums.begin(),nums.end());
+        //     sort(nums.begin(),nums.end()); // sort the array
 
-        //     int left =0;
-        //     int right = n-1;
+        //     int left =0; // first pointer at 0th index
+        //     int right = n-1; // second pointer at last index
 
         //     while(left < right){
-        //         if(nums[left] + nums[right] == target){
+        //         if(nums[left] + nums[right] == target){ // if sum is equal to target return true
         //             return true;
-        //         }else if(nums[left] + nums[right] > target){
-        //             right--;
-        //         }else{
-        //             left++;
+        //         }else if(nums[left] + nums[right] > target){ // if sum is greater then target
+        //             right--; // decrease right by 1
+        //         }else{ // if sum is less then target
+        //             left++; // increase left by 1
         //         }
         //     }
 
@@ -26,17 +26,17 @@ public:
         // ++++++++++ Better Solution (optimal solution if we need to return
         // index like this question) ++++++++++++++++++++++
         int n = nums.size();
-        map<int, int> mpp;
+        map<int, int> mpp; // create a map
 
         for (int i = 0; i < n; i++) {
-            int num = nums[i];
-            int require = target - num;
+            int num = nums[i]; // get current value
+            int require = target - num; // find the require value by minus current value from target value
 
-            if (mpp.find(require) != mpp.end()) {
-                return {mpp[require], i};
+            if (mpp.find(require) != mpp.end()) { // check of required value already present of not
+                return {mpp[require], i}; // if present the return that index with current value index
             }
 
-            mpp[num] = i;
+            mpp[num] = i; // put the index into map
         }
 
         return {};
