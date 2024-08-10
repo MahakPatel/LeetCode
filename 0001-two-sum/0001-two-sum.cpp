c078d2e0560a21451ef47e6de47688fc90e70cbc
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-08-09 21:54:00 -0500

    Time: 9 ms (68.65%), Space: 14.7 MB (14.07%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,27 +1,56 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n= nums.size();
+        // +++++++++++ Optimal Solution ( only if we need to return 'Yes' Or
+        // 'No') ++++++++
+        //     int n = nums.size();;
 
-        int j=0;
-        for(int i=0;i<n;i++){
-            j++;
-            if(nums[i]+nums[j]==target){
-                return {i,j};
+        //     sort(nums.begin(),nums.end());
+
+        //     int left =0;
+        //     int right = n-1;
+
+        //     while(left < right){
+        //         if(nums[left] + nums[right] == target){
+        //             return true;
+        //         }else if(nums[left] + nums[right] > target){
+        //             right--;
+        //         }else{
+        //             left++;
+        //         }
+        //     }
+
+        //     return false;
+        // }
+
+        // ++++++++++ Better Solution (optimal solution if we need to return
+        // index like this question) ++++++++++++++++++++++
+        int n = nums.size();
+        map<int, int> mpp;
+
+        for (int i = 0; i < n; i++) {
+            int num = nums[i];
+            int require = target - num;
+
+            if (mpp.find(require) != mpp.end()) {
+                return {mpp[require], i};
             }
+
+            mpp[num] = i;
         }
 
         return {};
-        
-    //     for(int i=0;i< n-1;i++){
-    //         for (int j=i+1;j<n;j++){
-    //         if(nums[i]+nums[j]== target){
-    //             return {i,j};
-    //         }
-        
-    //     }
-    // }
-    // return {}; 
-     
     }
+
+    // ++++++++++++ Brute Force +++++++++++++
+    //  int n = nums.size();
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = i + 1; j < n; j++) {
+    //             if (nums[i] + nums[j] == target) {
+    //                 return {i, j};
+    //             }
+    //         }
+    //     }
+    //     return {};
+    // }
 };
