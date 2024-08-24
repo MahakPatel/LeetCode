5da566b42e94a2f3f67bf67b97f068ef72689a86
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-08-24 16:16:22 -0500

    Time: 0 ms (100%), Space: 7.7 MB (7.99%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -12,5 +12,21 @@ public:
     int climbStairs(int n) {
         vector<int> memo(n + 1, -1); // Initialize memoization array with -1
         return climbStairs(n, memo);
+
+
+// ++++++++++++++++++ Bottom Up ++++++++++++++++++++++
+    //     if (n <= 1) return 1;
+        
+    //     int prev2 = 1; // Number of ways to climb n-2 stairs
+    //     int prev1 = 1; // Number of ways to climb n-1 stairs
+        
+    //     for (int i = 2; i <= n; ++i) {
+    //         int current = prev1 + prev2; // Number of ways to climb i stairs
+    //         prev2 = prev1; // Update prev2 to the previous step
+    //         prev1 = current; // Update prev1 to the current step
+    //     }
+        
+    //     return prev1;
+    // }
     }
 };
