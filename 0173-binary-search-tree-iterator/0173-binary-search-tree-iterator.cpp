fa6fbdd84387dd247f2a680f23b221eb55b2f3e0
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2024-09-24 22:50:51 -0500

    Time: 19 ms (77.51%), Space: 29.1 MB (90.41%) - LeetHub

--- 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
+++ 0173-binary-search-tree-iterator/0173-binary-search-tree-iterator.cpp
@@ -6,31 +6,35 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class BSTIterator {
-private : stack<TreeNode*> st; // Creating stack for storing node
+private:
+    stack<TreeNode*> st; // Creating stack for storing node
 public:
     BSTIterator(TreeNode* root) {
         pushAll(root); // push all the node
     }
-    
+
     int next() {
-        TreeNode* temp = st.top(); // create temp node and store top of the stack
+        TreeNode* temp =
+            st.top(); // create temp node and store top of the stack
         st.pop();
         pushAll(temp->right); // push all the node of "left node" of right mode
 
         return temp->val; // return temp node value
     }
-    
+
     bool hasNext() {
         return !st.empty(); // if stack is not emprt return true
     }
 
-private :
-    void pushAll(TreeNode *node){
-        for(;node != NULL; st.push(node),node=node->left); // push all the left side node into stack
+private:
+    void pushAll(TreeNode* node) {
+        for (; node != NULL; st.push(node), node = node->left)
+            ; // push all the left side node into stack
     }
 };
 
