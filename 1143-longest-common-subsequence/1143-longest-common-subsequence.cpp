445732944d4fe2c50f4f91ed503cc93684d63586
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-01-29 17:31:47 -0600

    Time: 15 ms (90.38%), Space: 9.2 MB (88.48%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -65,7 +65,8 @@ public:
     // int longestCommonSubsequence(string text1, string text2) {
     //     int index1 = text1.size(), index2 = text2.size();
     //     vector<vector<int>> dp(index1 + 1,
-    //                            vector<int>(index2 + 1, 0)); // Initialize with 0
+    //                            vector<int>(index2 + 1, 0)); // Initialize
+    //                            with 0
 
     //     // Build the DP table iteratively
     //     for (int i = 1; i <= index1; i++) {
@@ -74,7 +75,8 @@ public:
     //                 // Characters match, move diagonally
     //                 dp[i][j] = 1 + dp[i - 1][j - 1];
     //             } else {
-    //                 // Characters don't match, take the max of excluding either
+    //                 // Characters don't match, take the max of excluding
+    //                 either
     //                 // character
     //                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
     //             }
@@ -83,27 +85,23 @@ public:
 
     //     return dp[index1][index2]; // The last cell contains the answer
     // }
-  // ++++++++++++ Space Optimization ++++++++++++++ TC O(n*m)  SC O(n)
+    // ++++++++++++ Space Optimization ++++++++++++++ TC O(n*m)  SC O(m)
     int longestCommonSubsequence(string text1, string text2) {
-        int index1 = text1.size(), index2 = text2.size();
-        vector<int> prev(index1+1,0),cur(index2+1,0); // create variable to store prev and cur value
+        int n = text1.size(), m = text2.size();
 
-        // Build the DP table iteratively
-        for (int i = 1; i <= index1; i++) {
-            for (int j = 1; j <= index2; j++) {
+        vector<int> prev(m + 1, 0), cur(m + 1, 0); // Store only two rows
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
                 if (text1[i - 1] == text2[j - 1]) {
-                    // Characters match, move diagonally
-                    cur[j] = 1 + prev[j - 1];
+                    cur[j] = 1 + prev[j - 1]; // Move diagonally
                 } else {
-                    // Characters don't match, take the max of excluding either
-                    // character
-                    cur[j] = max(prev[j], cur[j - 1]);
+                    cur[j] = max(prev[j],cur[j - 1]); // Take max of excluding characters
                 }
-
-                prev = cur; // make prev to cur
             }
+            prev = cur; // Move current row to previous for the next iteration
         }
 
-        return prev[index2]; // The last cell contains the answer
+        return prev[m]; // The last element contains the answer
     }
 };
