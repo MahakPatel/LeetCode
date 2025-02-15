3b46c6826fe57caf273d96f024435ee04eadbcd4
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-02-15 00:59:41 -0600

    Time: 0 ms (100%), Space: 71.6 MB (80.68%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -2,29 +2,72 @@ class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
         //++++++++++++++++ Optiomal Solution +++++++++++++++++++++
-    // Initialize variables to track maximum sum found so far and current sum
-    int sum = 0;        // Current sum of subarray being considered
-    int maxi = INT_MIN; // Maximum sum found so far, initialized to smallest possible integer
+        // TC O(N)
+        // SC O(1)
+        // Initialize variables to track maximum sum found so far and current
+        // sum
+        int sum = 0;        // Current sum of subarray being considered
+        int maxi = INT_MIN; // Maximum sum found so far, initialized to smallest
+                            // possible integer
 
-    // Traverse through each element in the array
-    for (auto it : nums) {
-        // Add the current element to the current sum
-        sum += it;
-        
-        // Update maxi to be the maximum of current maxi and current sum
-        maxi = max(sum, maxi);
+        // Traverse through each element in the array
+        for (auto it : nums) {
+            // Add the current element to the current sum
+            sum += it;
 
-        // If current sum is less than zero, reset current sum to zero
-        // This effectively "resets" the subarray to start considering from the next element
-        if (sum < 0)
-            sum = 0;
+            // Update maxi to be the maximum of current maxi and current sum
+            maxi = max(sum, maxi);
+
+            // If current sum is less than zero, reset current sum to zero
+            // This effectively "resets" the subarray to start considering from
+            // the next element
+            if (sum < 0)
+                sum = 0;
+        }
+
+        // Return the maximum sum found
+        return maxi;
     }
 
-    // Return the maximum sum found
-    return maxi;
-}
+    // int maxSubArray(vector<int>& nums) {
+    //     //++++++++++++++++ Optiomal Solution +++++++++++++++++++++
+    //     // IF ASK FOR PRINT THE SUBARRAY
+    //     // TC O(N)
+    //     // SC O(1)
+    //     // Initialize variables to track maximum sum found so far and current
+    //     // sum
+    //     int sum = 0;        // Current sum of subarray being considered
+    //     int maxi = INT_MIN; // Maximum sum found so far, initialized to smallest
+    //                         // possible integer
+    //     int ansStart = -1;
+    //     int ansEnd = -1;
+
+    //     // Traverse through each element in the array
+    //     for (auto it : nums) {
+    //         if(sum == 0) start = i;
+    //         // Add the current element to the current sum
+    //         sum += it;
+
+    //         if(sum > max){
+    //             max = sum;
+    //             ansStart = start;
+    //             andEnd = i;
+    //         }
+
+    //         // If current sum is less than zero, reset current sum to zero
+    //         // This effectively "resets" the subarray to start considering from
+    //         // the next element
+    //         if (sum < 0)
+    //             sum = 0;
+    //     }
+
+    //     // Return the maximum sum found
+    //     return maxi;
+    // }
 
     // +++++++++++++++ Better Solution ++++++++++++++
+    // TC O(N^2)
+    // SC O(1)
     //     int n = nums.size();
     //     int maxi = INT_MIN;
 
@@ -41,6 +84,8 @@ public:
     // }
 
     // ++++++++++++++ Brute Force +++++++++++++++
+    // TC O(N^3)
+    // SC O(1)
     //     int n = nums.size();
     //     int maxi = INT_MIN;
 
