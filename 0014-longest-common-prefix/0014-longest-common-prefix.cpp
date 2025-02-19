e2467073c4002962da0b9884da1359a4593eaf80
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-02-19 01:42:42 -0600

    Time: 0 ms (100%), Space: 12.1 MB (37.07%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,22 +1,27 @@
 class Solution {
 public:
+    // ++++++++++++++ Optimal Solution +++++++++++++++
+    // TC O(nlogn + m) m = length of prefix
+    // SC O(1)
     string longestCommonPrefix(vector<string>& strs) {
         string ans = "";
-        sort(strs.begin(),strs.end());
+
+        // Sort the strings lexicographically
+        sort(strs.begin(), strs.end());
 
         int n = strs.size();
 
-        string first = strs[0],last = strs[n-1];
+        // First and last strings in sorted order
+        string first = strs[0], last = strs[n - 1];
 
-        for(int i=0;i<last.size();i++){
-            if(first[i] != last[i]){
-                return ans;
+        // Compare characters of first and last string
+        for (int i = 0; i < last.size(); i++) {
+            if (first[i] != last[i]) {
+                return ans; // Return common prefix found so far
             }
-
-            ans += first[i];
-
+            ans += first[i]; // Append matching character to result
         }
 
-        return ans;
+        return ans; // Return the longest common prefix
     }
 };
