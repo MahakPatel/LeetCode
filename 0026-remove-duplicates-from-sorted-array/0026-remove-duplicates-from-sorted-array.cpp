d1588b870ec02be090ea4bf8eff087d8eb4ae5c8
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-02-21 19:52:18 -0600

    Time: 0 ms (100%), Space: 22.7 MB (50.69%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -1,14 +1,41 @@
 class Solution {
 public:
+    // +++++++++++++ Optimal Solution +++++++++++++++
+    // TC O(n)
+    // SC O(1)
     int removeDuplicates(vector<int>& nums) {
         int j = 1;
         for (int i = 1; i < nums.size(); i++) {
             if (nums[i] != nums[i - 1]) { // check for not equal value
-                nums[j] = nums[i];        // if found than place to next index
+                nums[j] = nums[i];        // if found than place to next
                 j++;
             }
         }
 
         return j;
     }
+    // +++++++++++++ Brute Force +++++++++++++++
+    // TC O(n + nlogn)
+    // SC O(n)
+    // int removeDuplicates(vector<int>& nums) {
+    //     int n = nums.size();
+
+    //     set<int>
+    //         st; // Using a set to store unique elements (automatically sorted)
+
+    //     // Insert all elements into the set (O(N log N) due to set insertions)
+    //     for (int i = 0; i < n; i++) {
+    //         st.insert(nums[i]);
+    //     }
+
+    //     int index = 0;
+
+    //     // Copy unique elements back to the original array (O(N))
+    //     for (auto it : st) {
+    //         nums[index] = it;
+    //         index++;
+    //     }
+
+    //     return index; // Return the number of unique elements
+    // }
 };
