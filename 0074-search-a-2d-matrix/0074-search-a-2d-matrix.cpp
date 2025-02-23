a8e38e57b96126cdb675cd6ba7a2beb6a03cc4d8
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-02-23 16:33:08 -0600

    Time: 0 ms (100%), Space: 13.4 MB (43.11%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.cpp
@@ -1,63 +1,62 @@
 class Solution {
 public:
-
-//++++++++++++ Optimal Solution - Binary Search +++++++++
+    //++++++++++++ Optimal Solution - Binary Search +++++++++
+    // TC O(log(n*m))
+    // SC O(1)
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int low = 0;
         int n = matrix.size();
         int m = matrix[0].size();
 
-        int high =(n*m-1);
+        int high = (n * m - 1);
 
-        while(low <= high){
-            int mid = (low+high)/2;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            // convert 2D array to  1D array
+            int row = mid / m;
+            int column = mid % m;
 
-            int row = mid/m;
-            int column = mid%m;
-
-            if(matrix[row][column] == target){
+            if (matrix[row][column] == target) {
                 return true;
             }
 
-            if(matrix[row][column] <= target){
-                low = mid+1;
-            }else{
-                high = mid-1;
+            if (matrix[row][column] <= target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
             }
         }
 
         return false;
     }
 
+    //++++++++++++++++= Brute Force Approch ++++++++++++++
+    //     bool searchMatrix(vector<vector<int>>& matrix, int target) {
+    //     int n = matrix.size(), m = matrix[0].size();
 
-//++++++++++++++++= Brute Force Approch ++++++++++++++
-//     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-//     int n = matrix.size(), m = matrix[0].size();
+    //     //traverse the matrix:
+    //     for (int i = 0; i < n; i++) {
+    //         for (int j = 0; j < m; j++) {
+    //             if (matrix[i][j] == target)
+    //                 return true;
+    //         }
+    //     }
+    //     return false;
+    // }
 
-//     //traverse the matrix:
-//     for (int i = 0; i < n; i++) {
-//         for (int j = 0; j < m; j++) {
-//             if (matrix[i][j] == target)
-//                 return true;
-//         }
-//     }
-//     return false;
-// }
+    // +++++++++++ Better Solution ++++++++++++
 
+    // bool binarySearch(vector<int>& nums, int target) {
+    //     int n = nums.size(); //size of the array
+    //     int low = 0, high = n - 1;
 
-// +++++++++++ Better Solution ++++++++++++
-
-// bool binarySearch(vector<int>& nums, int target) {
-//     int n = nums.size(); //size of the array
-//     int low = 0, high = n - 1;
-
-//     // Perform the steps:
-//     while (low <= high) {
-//         int mid = (low + high) / 2;
-//         if (nums[mid] == target) return true;
-//         else if (target > nums[mid]) low = mid + 1;
-//         else high = mid - 1;
-//     }
-//     return false;
-// }
+    //     // Perform the steps:
+    //     while (low <= high) {
+    //         int mid = (low + high) / 2;
+    //         if (nums[mid] == target) return true;
+    //         else if (target > nums[mid]) low = mid + 1;
+    //         else high = mid - 1;
+    //     }
+    //     return false;
+    // }
 };
