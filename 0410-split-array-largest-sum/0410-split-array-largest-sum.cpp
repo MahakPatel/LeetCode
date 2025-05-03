51328b7e6e381b2229e5472686f07bf59b30449e
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-05-02 21:28:53 -0500

    Time: 0 ms (100%), Space: 10.5 MB (55.23%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.cpp
@@ -6,10 +6,13 @@ public:
         int numsOfSubArray = 1;
         int sumOfSubArr = 0;
         for (int i = 0; i < nums.size(); i++) {
-            if (sumOfSubArr + nums[i] <= sum) { // check if the sum of subarray and the sum of current element if less then the sum then sum them
+            if (sumOfSubArr + nums[i] <=
+                sum) { // check if the sum of subarray and the sum of current
+                       // element if less then the sum then sum them
                 sumOfSubArr = sumOfSubArr + nums[i];
             } else {
-                numsOfSubArray += 1; // or else increase thge subarray and put the sum in sumOfSubArr
+                numsOfSubArray += 1; // or else increase thge subarray and put
+                                     // the sum in sumOfSubArr
                 sumOfSubArr = nums[i];
             }
         }
@@ -19,14 +22,17 @@ public:
     int splitArray(vector<int>& nums, int k) {
         int n = nums.size();
 
-// range is between max to sum of the given elements
+        // range is between max to sum of the given elements
         int low = *max_element(nums.begin(), nums.end());
-        int high = accumulate(nums.begin(), nums.end(),0); // find the sum of given array
+        int high = accumulate(nums.begin(), nums.end(),
+                              0); // find the sum of given array
 
         while (low <= high) {
             int mid = (low + high) / 2;
 
-            if (numsOfSubArray(nums, mid) <= k) { // check if numsOfSubArray is less the. the given array then
+            if (numsOfSubArray(nums, mid) <=
+                k) { // check if numsOfSubArray is less the. the given array
+                     // then
                 high = mid - 1; // remove right portion of array
             } else {
                 low = mid + 1; // remove left portion of array
@@ -35,33 +41,4 @@ public:
 
         return low;
     }
-// +++++++++++++++ Brute Force Approch +++++++++++
-//     int countPartitions(vector<int> &a, int maxSum) {
-//     int n = a.size(); //size of array.
-//     int partitions = 1;
-//     long long subarraySum = 0;
-//     for (int i = 0; i < n; i++) {
-//         if (subarraySum + a[i] <= maxSum) {
-//             //insert element to current subarray
-//             subarraySum += a[i];
-//         }
-//         else {
-//             //insert element to next subarray
-//             partitions++;
-//             subarraySum = a[i];
-//         }
-//     }
-//     return partitions;
-// }
-
-// int largestSubarraySumMinimized(vector<int> &a, int k) {
-//     int low = *max_element(a.begin(), a.end());
-//     int high = accumulate(a.begin(), a.end(), 0);
-
-//     for (int maxSum = low; maxSum <= high; maxSum++) {
-//         if (countPartitions(a, maxSum) == k)
-//             return maxSum;
-//     }
-//     return low;
-// }
 };
