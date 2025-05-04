f493672f8a7f5f8fdcf7335b6c66fc9be0af39fa
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-05-04 18:14:15 -0500

    Time: 0 ms (100%), Space: 12.8 MB (78.15%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -6,14 +6,18 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(p == NULL || q == NULL) return (p==q);
+        if (p == NULL || q == NULL)
+            return (p == q);
         // apply any DFS method(here i apply preOrder travesal)
-        return (p->val == q->val) && isSameTree(p->left,q->left) && isSameTree(p->right,q->right); // check each node of p with each node of q at same time
+        return (p->val == q->val) && isSameTree(p->left, q->left) &&
+               isSameTree(p->right, q->right); // check each node of p with each
+                                               // node of q at same time
     }
 };
