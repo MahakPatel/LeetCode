e1cb05c2a17570a285683e44598a3a5c4e5ae86a
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-05-14 22:57:18 -0500

    Time: 0 ms (100%), Space: 52.3 MB (33.68%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool canJump(vector<int>& nums) {
+bool canJump(vector<int>& nums) {
         int maxLen = 0; // start with 0
         for (int i = 0; i < nums.size(); i++) {
             if (i > maxLen) // if index is greater then maxLen then return false
