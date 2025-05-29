c181f1610c85f6d8c7a76f317538ae599a0c5518
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-05-28 19:41:19 -0500

    Time: 6 ms (89.15%), Space: 11.9 MB (82.41%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,30 +1,6 @@
 class Solution {
 public:
-  // ++++++++++++ Tabulation ++++++++++++++ TC O(n*m)  SC O(n*m)
-    // int longestCommonSubsequence(string text1, string text2) {
-    //     int index1 = text1.size(), index2 = text2.size();
-    //     vector<vector<int>> dp(index1 + 1,
-    //                            vector<int>(index2 + 1, 0)); // Initialize
-    //                            with 0
-
-    //     // Build the DP table iteratively
-    //     for (int i = 1; i <= index1; i++) {
-    //         for (int j = 1; j <= index2; j++) {
-    //             if (text1[i - 1] == text2[j - 1]) {
-    //                 // Characters match, move diagonally
-    //                 dp[i][j] = 1 + dp[i - 1][j - 1];
-    //             } else {
-    //                 // Characters don't match, take the max of excluding
-    //                 either
-    //                 // character
-    //                 dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
-    //             }
-    //         }
-    //     }
-
-    //     return dp[index1][index2]; // The last cell contains the answer
-    // }
-    // ++++++++++++ Space Optimization ++++++++++++++ TC O(n*m)  SC O(m)
+// ++++++++++++ Space Optimization ++++++++++++++ TC O(n*m)  SC O(m)
     int longestCommonSubsequence(string text1, string text2) {
         int n = text1.size(), m = text2.size();
 
