0986b7fa48589368c2aacba45e4d0a186bae490b
Mahak Patel <71070264+MahakPatel@users.noreply.github.com>
2025-06-09 22:50:25 -0500

    Time: 0 ms (100%), Space: 15 MB (59.65%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -40,41 +40,6 @@ public:
         delete (deletedNode);
 
         return head;
-
-        // +++++++++++++++ Brute Force ++++++++++++ TC O(2N). SC O(1)
-        // int count =0;
-        // ListNode* temp = head;
-
-        // while(temp != NULL){ // find the length of LL
-        //     count++;
-        //     temp = temp -> next;
-        // }
-
-        // if(count == n ){ // if n == length of LL means the head of the LL
-        // then make the head->next is a new Head
-        //     ListNode* newHead = head -> next;
-        //     delete(head);
-
-        //     return newHead;
-        // }
-
-        // int resultNode = count - n; // total minus given number
-
-        // temp = head;
-
-        // while(temp!=NULL){
-        //     resultNode--;
-
-        //     if(resultNode == 0) break;
-
-        //     temp = temp->next;
-        // }
-
-        // ListNode* deletedNode = temp -> next; // for delete
-
-        // temp->next = temp->next->next; // previous node of deleted nod
-        // connect to next Node of deleted node delete(deletedNode);
-
-        // return head;
+        
     }
 };
